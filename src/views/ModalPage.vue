b1027621afedff2388eeb551c9c6a81d0b08cd75
Jamie Baker <jamie@affliction.digital>
2022-05-31 08:14:04 +1000

    Update to demonstrate problem with lifecycles

---
+++ src/views/ModalPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <p>This is a modal</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ModalPage",
+  components: {
+    IonContent,
+    IonPage,
+  },
+  ionViewDidEnter() {
+    console.log("This lifecycle hook should also work, but doesn't");
+  },
+});
+</script>
+
+<style scoped></style>
